d25e8a17ef43db182ab11e8f8102dbb675cbbea9
ViktorSveins <viktorsveins@gmail.com>
2020-01-28 22:53:03 +0000

    Walls and projectile bounce

---
+++ Assets/Scripts/ProjectileMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    Vector2 direction;
+    Rigidbody2D rb;
+    bool stop = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stop)
+        {
+            rb.velocity = new Vector2(Random.Range(-0.9f, 0.1f) * 100, Random.Range(-0.9f, 0.1f)) * 100;
+            stop = false;
+        }
+    }
+
+    void FixedUpdate() 
+    {
+    }
+}
