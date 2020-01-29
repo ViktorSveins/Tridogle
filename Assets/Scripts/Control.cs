80dfa72761f2a8fe0388fc3fe3a8d4217b892afc
ViktorSveins <viktorsveins@gmail.com>
2020-01-29 21:49:57 +0000

    loss state, sounds, projectile duplication

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public Transform projectilePrefab;
+    public Transform dogPrefab;
+    
+    public GameObject dog;
+    public GameObject walls;
+    public Camera cam;
+
+    Transform[] balls;
+
+    bool loosingState;
+    bool stop;
+    int projectiles;
+    Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stop = true;
+        projectiles = 0;
+        loosingState = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stop && !loosingState && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            dog.GetComponent<DogMovement>().enabled = true;
+            balls = new Transform[8];
+            balls[0] = Instantiate(projectilePrefab);
+            projectiles++;
+            stop = false;
+            color = cam.backgroundColor;
+        }
+        
+        int wallHits = walls.GetComponent<HitCounter>().hits;
+        if(projectiles < 2 && wallHits > 70)
+        {
+            projectiles = SpawnProjectiles(projectiles);
+        }
+        else if(projectiles < 3 && wallHits > 200)
+        {
+            projectiles = SpawnProjectiles(projectiles);
+        }
+        else if(projectiles < 5 && wallHits > 400)
+        {
+            projectiles = SpawnProjectiles(projectiles);
+        }
+        
+        if(dog.GetComponent<HitCounter>().hits == 32)
+        {
+            BlinkBackground();
+            Reset();
+        }
+    }
+
+    int SpawnProjectiles(int prior)
+    {
+        for(int i = 0; i < prior; i++)
+        {
+            balls[i + prior] = Instantiate(projectilePrefab, balls[i].position, Quaternion.identity);
+        }
+        return prior * 2;
+    }
+
+    void Reset() {
+        for(int i = 0; i < projectiles; i++)
+        {
+            Destroy(balls[i].gameObject);
+        }
+        Destroy(dog);
+        walls.GetComponent<HitCounter>().hits = 0;
+
+        stop = true;
+
+        dog = Instantiate(dogPrefab).gameObject;
+        projectiles = 0;
+    }
+
+    void BlinkBackground() {
+        loosingState = true;
+        StartCoroutine(Blink());
+    }
+    
+    IEnumerator Blink() {
+        for (int i=0; i<12; i++) {
+            if(cam.backgroundColor == color)
+            {
+                cam.backgroundColor = new Color(0.67f, 0.278f, 0.224f);
+            }
+            else
+            {
+                cam.backgroundColor = color;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+        cam.backgroundColor = color;
+        loosingState = false;
+    }
+}
--- Assets/Scripts/ProjectileMovement.cs
+++ Assets/Scripts/ProjectileMovement.cs
@@ -6,20 +6,52 @@ public class ProjectileMovement : MonoBehaviour
 {
     Vector2 direction;
     Rigidbody2D rb;
+    float maxMagnitude;
+    float magLessener;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        direction = new Vector2(Random.Range(-200.0f, -100.0f), Random.Range(-200.0f, -100.0f));
+        direction = new Vector2(Random.Range(-100.0f, -50.0f), Random.Range(-100.0f, -50.0f));
         rb.velocity = direction;
+        maxMagnitude = 300.0f;
+        magLessener = maxMagnitude * 0.75f;
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    void OnCollisionExit2D(Collision2D col)
     {
-        rb.velocity *= 1.1f; 
-        if(col.gameObject.tag == "Wall")
+        float magnitude = rb.velocity.magnitude;
+        float change = Random.Range(10.0f, 50.0f);
+        
+        if(rb.velocity.x < 100.0f && rb.velocity.x > -100.0f)
         {
+            if(rb.velocity.x < 0)
+            {
+                rb.velocity -= new Vector2(change, 0);
+            }
+            else 
+            {
+                rb.velocity += new Vector2(change, 0);
+            }
+        }
+        else if(rb.velocity.y < 100.0f && rb.velocity.y > -100.0f)
+        {
+            if(rb.velocity.y < 0)
+            {
+                rb.velocity -= new Vector2(0, change);
+            }
+            else 
+            {
+                rb.velocity += new Vector2(0, change);
+            }
+        }
+        else 
+        {
+            Vector2 normal = rb.velocity.normalized;
+            float thrustFactor = 1.0f + (maxMagnitude - magnitude) / magLessener;
+
+            rb.velocity *= thrustFactor;
         }
     }
 }
